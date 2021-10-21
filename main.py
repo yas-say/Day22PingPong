cf03f8de28b02dea11677ceadbb75bfac31a7061
YMOHANTY <yasowant@live.com>
2021-10-20 23:17:09 -0400

    ball class

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from turtle import Turtle, Screen
 from paddle import Paddle
-
+from ball import Ball
+from time import sleep
 screen = Screen()
 screen.setup(800, 600)
 screen.bgcolor("black")
@@ -11,7 +12,7 @@ lcordinate = (-350, 0)
 rcordinate = (350, 0)
 lpaddle = Paddle(lcordinate)
 rpaddle = Paddle(rcordinate)
-
+ball = Ball()
 screen.listen()
 
 screen.onkey(rpaddle.go_up, "Up")
@@ -21,5 +22,8 @@ screen.onkey(lpaddle.go_down, "s")
 
 flag = True
 while flag:
+    sleep(0.1)
+    ball.move()
     screen.update()
+
 screen.exitonclick()
